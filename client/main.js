bccd5046313f295f9813f404e525bdb1be51e9dc
odulzaides <dulzaides.oscar@gmail.com>
2016-04-26 14:13:23 -0400

    Added modal to warn if user is not logged in. Owner field added to DB insert. Dropdown added for sorting, js needs to be written

--- client/main.js
+++ client/main.js
@@ -10,6 +10,9 @@ Template.registerHelper('formatDate', function(date) {
     });
 
 });
+Template.add_task.rendered = function() {
+    $('#my-datepicker').datepicker();
+}
 Template.item_list.helpers({
     items: function() {
         return Items.find();
@@ -18,12 +21,16 @@ Template.item_list.helpers({
 });
 
 /// //// ////
-////	events
+////    events
 /// //// ////
 Template.body.events({
     'click .js-add-task-form': function(event) {
-    	console.log('clicked');
-        $("#task_add_form").modal('show');
+        if (!Meteor.user()) {
+            $("#join_or_login").modal('show');
+        } else {
+            console.log('clicked');
+            $("#task_add_form").modal('show');
+        }
     }
 });
 
@@ -31,11 +38,13 @@ Template.add_task.events({
     'submit .js-emoticon': function(event) {
         event.preventDefault();
         // console.log('clicked');
-        // var text = event.target.text.value;	
+        // var text = event.target.text.value;  
         // $('#text_display').html(text);
         // $('#text_display').emoticonize();
         var task = event.target.text.value;
-        Meteor.call("addTask", task);
+        var due = $('#my-datepicker').val();
+        var priority = event.target.priority.value;
+        Meteor.call("addTask", task, due, priority);
         $("#task_add_form").modal('hide');
     }
 });
